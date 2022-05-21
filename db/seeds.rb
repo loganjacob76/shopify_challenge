a5128875b9e24e841534a7baff4ddaf56fe9696c
loganjacob76 <nagolisback@gmail.com>
2022-05-20 18:20:04 -0600

    Add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+4.times do
+  Beer.create(name: Faker::Beer.name, inventory: Faker::Number.within(range: 5..25))
+end
+
+Beer.create(name: 'Deleted', inventory: Faker::Number.within(range: 5..25), active: false)
