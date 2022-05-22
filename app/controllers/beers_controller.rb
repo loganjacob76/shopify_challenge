5e151302c9d5b3c06cc9a5ac23eff9a7b4b09e59
loganjacob76 <nagolisback@gmail.com>
2022-05-21 23:23:04 -0600

    Add 'new' logic & strong params

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -2,4 +2,17 @@ class BeersController < ApplicationController
   def index
     @beers = Beer.in_use
   end
+
+  def new
+    @beer = Beer.new
+  end
+
+  def create
+    
+  end
+
+  private
+  def beer_params
+    params.require(:beer).permit(:name, :inventory)
+  end
 end
